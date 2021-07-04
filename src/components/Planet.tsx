87fff8aea87cb405c23f6f277c537b70c26cc6d0
Drew <glavich_drew@yahoo.com>
2021-07-04 18:50:23 +0900

    mutate planets error

--- src/components/Planet.tsx
+++ src/components/Planet.tsx
@@ -8,28 +8,31 @@ interface IPlanet {
 }
 
 const setPlanetDoc = async (planet: TPlanet) => {
-  try {
-    const docRef = await db.collection('planets').add(planet);
-    console.info('Document written with ID: ', docRef.id);
-  } catch (error) {
-    console.error('Error adding document: ', error);
-  }
+  const docRef = await db.collection('planets').add(planet);
+  console.info('Document written with ID: ', docRef.id);
 };
 
 const Planet: React.FC<IPlanet> = ({ planet }) => {
   const mutation = useMutation(setPlanetDoc);
+
   return (
     <div className="card">
       <h3>{planet.name}</h3>
       <p>Population - {planet.population}</p>
       <p>Terrain - {planet.terrain}</p>
       <p>Climate - {planet.climate}</p>
-      <button
-        onClick={() => mutation.mutate(planet)}
-        disabled={mutation.isLoading}
-      >
-        Save to database
-      </button>
+      {!mutation.error ? (
+        <button
+          onClick={() => mutation.mutate(planet)}
+          disabled={mutation.isLoading}
+        >
+          Save to database
+        </button>
+      ) : (
+        <button onClick={() => mutation.reset()} className="error">
+          Reset because of error
+        </button>
+      )}
     </div>
   );
 };
